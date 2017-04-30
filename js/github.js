97baca9c74f77b3bda445aba820046c2ff0b2360
yacine96 <yacineazouani96@gmail.com>
2017-04-30 13:49:25 +0200

    Update github.js

--- js/github.js
+++ js/github.js
@@ -9,13 +9,5 @@ var formElt = document.querySelector("form");
 formElt.addEventListener("submit", function (e) {
     e.preventDefault();
     var nomProfil = formElt.elements.profil.value;
-    var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
-    }
+    ajaxGet("https://api.github.com/users/" + nomProfil);
 };
-xmlhttp.open("GET", "https://bitcoin.mubiz.com/"+ nomProfil, true);
-xmlhttp.send();
