e863e682d2dd0416b36a14360497b53c0d0053a4
yacine96 <yacineazouani96@gmail.com>
2017-04-30 18:50:19 +0200

    Update github.js

--- js/github.js
+++ js/github.js
@@ -1,7 +1,26 @@
+        
+/* Ici la fonction traitant les requetes du TP1*/
 
-/*
-Exercice : profil GitHub
-*/
+var formElt = document.querySelector("form");
+formElt.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var nomProfil = formElt.elements.profil.value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById("demo").innerHTML = jsonPretty;
+        }
+    };
+    var nomProfil=formElt.elements.profil.value;
+    xmlhttp.open("GET", "https://bitcoin.mubiz.com/"+nomProfil, true);
+    xmlhttp.send();
+    
+});
+
+/*Essai de code d'une des fonctions correspondant à la requete Block Hash mais elle ne fonctionne pas très bien*/
+
 function afficheBlockhash(){
 var formElt = document.querySelector("form");
 formElt.addEventListener("submit", function (e) {
@@ -22,20 +41,3 @@ formElt.addEventListener("submit", function (e) {
 });
 }
 
-var formElt = document.querySelector("form");
-formElt.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var nomProfil = formElt.elements.profil.value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myObj = this.responseText;
-            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-            document.getElementById("demo").innerHTML = jsonPretty;
-        }
-    };
-    var nomProfil=formElt.elements.profil.value;
-    xmlhttp.open("GET", "https://bitcoin.mubiz.com/"+nomProfil, true);
-    xmlhttp.send();
-    
-});
