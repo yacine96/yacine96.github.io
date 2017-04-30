5f2300227799b34198fa4304e6b7767f8f193a3b
yacine96 <yacineazouani96@gmail.com>
2017-04-30 13:19:46 +0200

    Update test.js

--- java/test.js
+++ java/test.js
@@ -10,27 +10,13 @@ var formElt = document.querySelector("form");
 formElt.addEventListener("submit", function (e) {
     e.preventDefault();
     var nomProfil = formElt.elements.profil.value;
-    ajaxGet("https://api.github.com/users/" + nomProfil, function (reponse) {
-        // Transformation de la réponse en un objet JSON
-        var profil = JSON.parse(reponse);
-        // Création des informations sur le profil
-        var avatarElt = document.createElement("img");
-        avatarElt.src = profil.avatar_url;
-        avatarElt.style.height = "150px";
-        avatarElt.style.width = "150px";
-        var nomElt = document.createElement("div");
-        nomElt.textContent = profil.name;
-        nomElt.style.fontSize = "20px";
-        var employeurElt = document.createElement("div");
-        employeurElt.textContent = profil.company;
-        var siteElt = document.createElement("a");
-        siteElt.href = profil.blog;
-        siteElt.textContent = siteElt.href;
-        // Affichage des informations
-        infosElt.innerHTML = ""; // Suppression des infos précédentes
-        infosElt.appendChild(avatarElt);
-        infosElt.appendChild(nomElt);
-        infosElt.appendChild(employeurElt);
-        infosElt.appendChild(siteElt);
-    });
-});
+    var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://bitcoin.mubiz.com/"+ nomProfil, true);
+xmlhttp.send();
