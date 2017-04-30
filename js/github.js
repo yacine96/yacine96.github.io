636fca09c4761f2446e8ddf4b0a3795a4b023d2c
yacine96 <yacineazouani96@gmail.com>
2017-04-30 14:05:20 +0200

    Update github.js

--- js/github.js
+++ js/github.js
@@ -3,11 +3,20 @@
 Exercice : profil GitHub
 */
 
-var infosElt = document.getElementById("infos");
 
 var formElt = document.querySelector("form");
 formElt.addEventListener("submit", function (e) {
     e.preventDefault();
     var nomProfil = formElt.elements.profil.value;
-    ajaxGet("https://api.github.com/users/" + nomProfil);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById("demo").innerHTML = jsonPretty;
+        }
+    };
+    xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+    xmlhttp.send();
+    
 });
