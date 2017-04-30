5727770feb9332cc67b2d2f154758e7b466ef714
yacine96 <yacineazouani96@gmail.com>
2017-04-30 18:37:24 +0200

    Update github.js

--- js/github.js
+++ js/github.js
@@ -2,7 +2,25 @@
 /*
 Exercice : profil GitHub
 */
-
+function afficheBlockhash(){
+var formElt = document.querySelector("form");
+formElt.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var blockhash = formElt.elements.inputBlock.value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById("demo1").innerHTML = jsonPretty;
+        }
+    };
+    var nomProfil=formElt.elements.inputBlock.value;
+    xmlhttp.open("GET", "https://bitcoin.mubiz.com/block_hash/"+blockhash, true);
+    xmlhttp.send();
+    
+});
+}
 
 var formElt = document.querySelector("form");
 formElt.addEventListener("submit", function (e) {
